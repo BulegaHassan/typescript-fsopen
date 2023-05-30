59f5c52d70b073e3e3686ff77444a8264a11d679
Hassan Bulega <hassanbulega@gmail.com>
2023-05-30 14:00:55 +0300

    add flight content

---
+++ flights/frontend-flights/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const App = () => {
+  const [newNote, setNewNote] = useState("");
+  const [notes, setNotes] = useState([]);
+
+  return null;
+};
+export default App;
